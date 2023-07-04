afa4edd13d8f9fd302802361d2418c9c9f02dc56
PierreAntoineBourquin <pabourquin@infomaniak.ch>
2023-07-04 11:51:05 +0200

    Ajout de la page paleb.html

--- merche/listings/views.py
+++ merche/listings/views.py
@@ -1,8 +1,8 @@
-
-
 from django.http import HttpResponse
 from listings.models import Band
+from listings.models import Paleb
 from django.shortcuts import render
+
 
 def hello(request):
     bands = Band.objects.all()
@@ -10,14 +10,21 @@ def hello(request):
                   'listings/hello.html',
                   {'bands': bands})
 
+
 def about(request):
     return HttpResponse("<h1>À propos</h1><p> Nous adorons merch !</p>")
+
 
 def contact(request):
     return HttpResponse("<h1>Contact</h1><p> Contact</p>")
 
+
 def listings(request):
     return HttpResponse("<h1>Listings</h1><p> Formulaire de listings</p>")
 
+
 def paleb(request):
-    return HttpResponse("<h1>paleb</h1><p> page paleb</p>")
+    elementspaleb = Paleb.objects.all()
+    return render(request,
+                  'listings/paleb.html',
+                  {'elementspaleb': elementspaleb})
